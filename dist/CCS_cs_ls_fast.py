a53caf720cf54ae79f6cb705e326152b8fabfdf7
gymmer <511470840@qq.com>
2018-03-23 10:35:48 +0800

    v93

--- dist/CCS_cs_ls_fast.py
+++ dist/CCS_cs_ls_fast.py
@@ -8,27 +8,33 @@ N = 512                     # 训练序列长度/载波数,满足：L<=N
 P = 36                      # CS估计导频数，P<N
 pos_ls = arange(0,N,5)      # LS估计的均匀导频图样
 
-CS_MSE = array([-12.91947905, -18.41703987, -23.00347076, -28.11697554,
-       -33.29921024, -37.75578647, -42.8740129 ])
+bob_MSE = array([[-12.91947905, -9.86169658],
+       [-18.41703987, -12.14495089],
+       [-23.00347076, -13.4433354 ],
+       [-28.11697554, -13.64853738],
+       [-33.29921024, -13.40936531],
+       [-37.75578647, -14.02741247],
+       [-42.8740129, -13.89044002]])
 
-CS_BER = array([ 0.11544643,  0.04758403,  0.01915966,  0.00629727,  0.00182248,
-        0.00061975,  0.00027311])
+bob_BER = array([[ 0.11544643,  0.15578851],
+       [ 0.04758403,  0.10189487],
+       [ 0.01915966,  0.0740709 ],
+       [ 0.00629727,  0.06553178],
+       [ 0.00182248,  0.06363692],
+       [ 0.00061975,  0.05954768],
+       [ 0.00027311,  0.06026284]])
 
-LS_MSE = array([ -9.86169658, -12.14495089, -13.4433354 , -13.64853738,
-       -13.40936531, -14.02741247, -13.89044002])
-
-LS_BER = array([ 0.15578851,  0.10189487,  0.0740709 ,  0.06553178,  0.06363692,
-        0.05954768,  0.06026284])
-
-CS_SC = array([ 0.49381484,  0.73277615,  0.8679723 ,  0.94639922,  0.98128938,
-        0.99229593,  0.99622225])
-
-LS_SC = array([ 0.00328107, -0.01683116,  0.00695385, -0.00864503, -0.00594933,
-        0.02370059, -0.00297679])
+SC = array([[ 0.49381484,  0.00328107],
+       [ 0.73277615, -0.01683116],
+       [ 0.8679723,   0.00695385],
+       [ 0.94639922, -0.00864503],
+       [ 0.98128938, -0.00594933],
+       [ 0.99229593,  0.02370059],
+       [ 0.99622225,  0.00297679]])
 
 plt.figure(figsize=(8,5))
-plt.plot(SNR,CS_MSE,'ko-',label='CS(Random, P=%s)'%(P))
-plt.plot(SNR,LS_MSE,'k^:',label='LS(Even, P=%s)'%(len(pos_ls)))
+plt.plot(SNR,bob_MSE[:,0],'ko-',label='CS(Random, P=%s)'%(P))
+plt.plot(SNR,bob_MSE[:,1],'k^:',label='LS(Even, P=%s)'%(len(pos_ls)))
 plt.xlabel('SNR(dB)')
 plt.ylabel('MSE(dB)')
 plt.title('MSE')
@@ -36,8 +42,8 @@ plt.legend()
 plt.show()
 
 plt.figure(figsize=(8,5))
-plt.semilogy(SNR,CS_BER,'ko-',label='CS(Random, P=%s)'%(P))
-plt.semilogy(SNR,LS_BER,'k^:',label='LS(Even, P=%s)'%(len(pos_ls)))
+plt.semilogy(SNR,bob_BER[:,0],'ko-',label='CS(Random, P=%s)'%(P))
+plt.semilogy(SNR,bob_BER[:,1],'k^:',label='LS(Even, P=%s)'%(len(pos_ls)))
 plt.xlabel('SNR(dB)')
 plt.ylabel('BER')
 plt.title('BER')
@@ -45,8 +51,8 @@ plt.legend()
 plt.show()
 
 plt.figure(figsize=(8,5))
-plt.plot(SNR,CS_SC,'ko-',label='CS(Random, P=%s)'%(P))
-plt.plot(SNR,LS_SC,'k^:',label='LS(Even, P=%s)'%(len(pos_ls)))
+plt.plot(SNR,SC[:,0],'ko-',label='CS(Random, P=%s)'%(P))
+plt.plot(SNR,SC[:,1],'k^:',label='LS(Even, P=%s)'%(len(pos_ls)))
 plt.xlabel('SNR(dB)')
 plt.ylabel('Capacity(bit/symbol)')
 plt.title('Security Capacity')
--- dist/CCS_cs_random_even_fast.py
+++ dist/CCS_cs_random_even_fast.py
@@ -1,46 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
-from numpy import arange,array
+from numpy import array
 
 SNR = range(0,31,5)
-N = 512                     # 训练序列长度/载波数,满足：L<=N
-P = 36                      # CS估计导频数，P<N
-pos_even = arange(1,N,15)   # 均匀导频图样
+ptype = ['Random', 'Even']
 
-random_MSE = array([
-    -13.7467857 , -17.48676158, -21.97246281, -27.64967497,
-    -32.73598317, -38.43057278, -42.65321231
-])
+bob_MSE = array([[-13.7467857,  -8.92763373],
+       [-17.48676158, -10.86919189],
+       [-21.97246281, -12.61358271],
+       [-27.64967497, -14.9618872],
+       [-32.73598317, -17.86330612],
+       [-38.43057278, -18.69453062],
+       [-42.65321231, -18.88381722]])
 
-even_MSE = array([
-    -8.92763373, -10.86919189, -12.61358271, -14.9618872 ,
-    -17.86330612, -18.69453062, -18.88381722
-])
+bob_BER = array([[ 0.10734769,  0.19254717],
+       [ 0.05466912,  0.1684696 ],
+       [ 0.0216229,   0.15188155],
+       [ 0.00681723,  0.14548742],
+       [ 0.0021166,   0.12676101],
+       [ 0.00064076,  0.14462264],
+       [ 0.00015231,  0.1440566]])
 
-random_BER = array([
-    0.10734769,  0.05466912,  0.0216229 ,  0.00681723,  0.0021166 ,
-    0.00064076,  0.00015231
-])
-
-even_BER = array([
-    0.19254717,  0.1684696 ,  0.15188155,  0.14548742,  0.12676101,
-    0.14462264,  0.1440566
-])
-
-random_SC = array([
-    0.52319569,  0.70209723,  0.8530099 ,  0.94307637,  0.97851096,
-    0.99227858,  0.99737562
-])
-
-even_SC = array([
-    0.03976407, -0.04830166,  0.02344127, -0.00486084, -0.0116633 ,
-    0.00560601, -0.03364716
-])
+SC = array([[ 0.52319569,  0.03976407],
+       [ 0.70209723, -0.04830166],
+       [ 0.8530099 ,  0.02344127],
+       [ 0.94307637, -0.00486084],
+       [ 0.97851096, -0.0116633 ],
+       [ 0.99227858,  0.00560601],
+       [ 0.99737562, -0.03364716]])
 
 plt.figure(figsize=(8,5))
-plt.plot(SNR,random_MSE,'ko-',label='Random')
-plt.plot(SNR,even_MSE,  'k^:',label='Even')
+plt.plot(SNR,bob_MSE[:,0],'ko-',label=ptype[0])
+plt.plot(SNR,bob_MSE[:,1],'k^:',label=ptype[1])
 plt.xlabel('SNR(dB)')
 plt.ylabel('MSE(dB)')
 plt.title('MSE')
@@ -48,8 +40,8 @@ plt.legend()
 plt.show()
 
 plt.figure(figsize=(8,5))
-plt.semilogy(SNR,random_BER,'ko-',label='Random')
-plt.semilogy(SNR,even_BER,  'k^:',label='Even')
+plt.semilogy(SNR,bob_BER[:,0],'ko-',label=ptype[0])
+plt.semilogy(SNR,bob_BER[:,1],'k^:',label=ptype[1])
 plt.xlabel('SNR(dB)')
 plt.ylabel('BER')
 plt.title('BER')
@@ -57,8 +49,8 @@ plt.legend()
 plt.show()
 
 plt.figure(figsize=(8,5))
-plt.plot(SNR,random_SC,'ko-',label='Random')
-plt.plot(SNR,even_SC,  'k^:',label='Even')
+plt.plot(SNR,SC[:,0],'ko-',label=ptype[0])
+plt.plot(SNR,SC[:,1],'k^:',label=ptype[1])
 plt.xlabel('SNR(dB)')
 plt.ylabel('Capacity(bit/symbol)')
 plt.title('Security Capacity')
